62ee0a97e9c1e09a5bfbe819e433a1883f598ed8
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2023-10-19 10:25:41 -0300

    Add files via upload

---
+++ 1920_IntroducaoPHP/crud/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body class="bg-dark text-info text-center">
+    <div class="container mt-2">
+        <div class="row">
+            <a class="col-sm-2 btn btn-info fw-bold" href="incluir.php">Incluir</a>
+            <a class="col-sm-2 btn btn-danger fw-bold" href="exclui.php">Excluir</a>
+            <a class="col-sm-2 offset-sm-1 btn btn-primary fw-bold" href="index.php">&#127968;</a>
+            <a class="col-sm-2 offset-sm-1 btn btn-warning fw-bold" href="alterar.php">Alterar</a>
+            <a class="col-sm-2 btn btn-success fw-bold" href="exibir.php">Exibir</a>
+        </div>
+        <div class="row mt-3 mb-3">
+            <h1 class="col-sm-12 fs-1 fw-bold text-primary">CRUD com Apache, MySQL e PHP</h1>
+        </div>
+        <div class="row mt-1" id="painel">
+            <label id="logo" class="bg-info col-sm-12 text-white fw-bold rounded-3 mt-5"
+            style="font-size:300%;">ADS - Estácio</label>
+        </div>
+    </div>
+</body>
+</html>
