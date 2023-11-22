0fdfcc53fb0cd6b84be21883a2bb2c39ec46b279
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2023-11-22 19:07:48 -0300

    Add files via upload

---
+++ ExercíciosP2/SASRResultPi.js
@@ -0,0 +1,9 @@
+//3)escreva uma função que retorna o nº pi
+//Função sem argumento e com retorno
+function retornaPiComRetorno() {
+    return Math.PI;
+}
+
+//Chamando a função e exibindo o retorno
+//exibindo o valor retornado no console
+console.log(retornaPiComRetorno());
