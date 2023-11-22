16539cdffe3b5fa6fef39e1f0e797f47b6c38789
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2023-11-22 18:46:33 -0300

    Update SASR.js

--- ExercíciosP2/SASR.js
+++ ExercíciosP2/SASR.js
@@ -1,9 +1,9 @@
+//1) escreva em js uma função sem argumento e sem retorno
 //Função sem argumento e sem retorno
 //Não aceita argumento,conforme indicado pelos parênteses vazios
 function texto() {
     //imprime a mensagem
     console.log("Tenha um bom dia para todos vocês");
 }
-
 //Chamando a função
 texto();
